5b7a45bede8031c51ac9a9aed1da2da78c7a3ad5
lsoul1027@gmail.com <lsoul1027@gmail.com>
2025-03-13 17:58:43 +0900

    신뢰 풀어야함

---
+++ 신뢰.py
@@ -0,0 +1,21 @@
+T = int(input()) # 테스트를 끝낼 수 있는 가장 빠른 시간
+# 버튼 앞이 아니라면 이동하는 변수
+def move_to_button():
+
+
+
+def click_button:
+
+    idx += 1
+
+
+for tc in range(1,1+T):
+    button_decimal = list(input().split()) #
+    N = button_decimal.pop(0) # N 따로 빼서 할당, 리스트는 자연스레 버튼,로봇 종류 순서로 정렬
+
+    check_po_count = 0 # 테스트에 있는 버튼만큼 버튼을 눌렀는지 확인해 줄 변수
+    time = 0 # 시간을 넣어줄 변수
+    idx = 0 # 테스트 리스트 순회해줄 idx
+    while check_po_count < N:
+
+
